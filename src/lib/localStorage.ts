9adf3c1bfb00316232424a22458eea09f317fad3
David Inoa <davidinoa@gmail.com>
2023-09-20 09:17:05 -0400

    Check window is defined before using localStorage

--- src/lib/localStorage.ts
+++ src/lib/localStorage.ts
@@ -1,6 +1,7 @@
 const highContrastKey = 'highContrast'
 
 export const setStoredIsHighContrastMode = (isHighContrast: boolean) => {
+  if (typeof window === 'undefined') return
   if (isHighContrast) {
     localStorage.setItem(highContrastKey, '1')
   } else {
@@ -9,6 +10,7 @@ export const setStoredIsHighContrastMode = (isHighContrast: boolean) => {
 }
 
 export const getStoredIsHighContrastMode = () => {
+  if (typeof window === 'undefined') return
   const highContrast = localStorage.getItem(highContrastKey)
   return highContrast === '1'
 }
