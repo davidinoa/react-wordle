79964a3bc1f0ae3f039a3a18692798795eb09ad5
David Inoa <davidinoa@gmail.com>
2023-09-21 10:28:02 -0400

    Update string constants

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,2 +1,10 @@
-export const ENTER_TEXT = 'Enter'
+export const CORRECT_WORD_MESSAGE = (solution: string) =>
+  `The word was ${solution}`
 export const DELETE_TEXT = 'Delete'
+export const ENTER_TEXT = 'Enter'
+export const NOT_CONTAINED_MESSAGE = (letter: string) =>
+  `Guess must contain ${letter}`
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
+export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
+  `Must use ${guess} in position ${position}`
