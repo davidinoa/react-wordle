d55bb901389f7c2dd5388e5b0bbd6237bd35dc76
David Inoa <davidinoa@gmail.com>
2023-09-22 09:29:29 -0400

    Implement a modal for the settings

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -4,6 +4,9 @@ export const DELETE_TEXT = 'Delete'
 export const ENTER_TEXT = 'Enter'
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
   `Guess must contain ${letter}`
+export const HARD_MODE_DESCRIPTION =
+  'Any revealed hints must be used in subsequent guesses'
+export const HIGH_CONTRAST_MODE_DESCRIPTION = 'For improved color vision'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
 export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
