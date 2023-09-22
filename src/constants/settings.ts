6afddf59d6c1669f83c21b086551e56761e4e890
David Inoa <davidinoa@gmail.com>
2023-09-22 09:45:23 -0400

    Implement stats related components

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,4 +1,6 @@
 export const ALERT_TIME_MS = 2000
+export const BLOWFISH_KEY = 'xcQUAHsik#Thq&LG*8es2DsZ$3bw^e'
+export const BLOWFISH_IV = '#45XmF^w'
 export const ENABLE_ARCHIVED_GAMES = false
 export const MAX_CHALLENGES = 6
 export const REVEAL_TIME_MS = 350
--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,5 +1,7 @@
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
   `The word was ${solution}`
+export const BEST_STREAK_TEXT = 'Best streak'
+export const CURRENT_STREAK_TEXT = 'Current streak'
 export const DELETE_TEXT = 'Delete'
 export const ENTER_TEXT = 'Enter'
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
@@ -7,7 +9,12 @@ export const NOT_CONTAINED_MESSAGE = (letter: string) =>
 export const HARD_MODE_DESCRIPTION =
   'Any revealed hints must be used in subsequent guesses'
 export const HIGH_CONTRAST_MODE_DESCRIPTION = 'For improved color vision'
+export const MIGRATE_BUTTON_TEXT = 'Transfer'
+export const MIGRATE_DESCRIPTION_TEXT =
+  'Click here to transfer your statistics to a new device.'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
+export const SUCCESS_RATE_TEXT = 'Success rate'
+export const TOTAL_TRIES_TEXT = 'Total tries'
 export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
   `Must use ${guess} in position ${position}`
