1f945af163321cc95d5f5c5f21497c1c02283756
David Inoa <davidinoa@gmail.com>
2023-09-22 10:02:32 -0400

    Create a modal for the stats

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -5,5 +5,6 @@ export const BLOWFISH_KEY = 'xcQUAHsik#Thq&LG*8es2DsZ$3bw^e'
 export const BLOWFISH_IV = '#45XmF^w'
 export const DATE_LOCALE = enUS
 export const ENABLE_ARCHIVED_GAMES = false
+export const ENABLE_MIGRATE_STATS = true
 export const MAX_CHALLENGES = 6
 export const REVEAL_TIME_MS = 350
--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -8,15 +8,22 @@ export const DATEPICKER_TITLE = 'Choose a past date'
 export const DATEPICKER_TODAY_TEXT = 'today'
 export const DELETE_TEXT = 'Delete'
 export const ENTER_TEXT = 'Enter'
-export const NOT_CONTAINED_MESSAGE = (letter: string) =>
-  `Guess must contain ${letter}`
+export const GAME_TITLE = 'React Wordle'
+export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
 export const HARD_MODE_DESCRIPTION =
   'Any revealed hints must be used in subsequent guesses'
 export const HIGH_CONTRAST_MODE_DESCRIPTION = 'For improved color vision'
 export const MIGRATE_BUTTON_TEXT = 'Transfer'
 export const MIGRATE_DESCRIPTION_TEXT =
   'Click here to transfer your statistics to a new device.'
+export const NEW_WORD_TEXT = 'New word in'
+export const NOT_CONTAINED_MESSAGE = (letter: string) =>
+  `Guess must contain ${letter}`
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
+export const SHARE_FAILURE_TEXT =
+  'Unable to share the results. This feature is available only in secure contexts (HTTPS), in some or all supporting browsers.'
+export const SHARE_TEXT = 'Share'
+export const STATISTICS_TITLE = 'Statistics'
 export const SUCCESS_RATE_TEXT = 'Success rate'
 export const TOTAL_TRIES_TEXT = 'Total tries'
 export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
